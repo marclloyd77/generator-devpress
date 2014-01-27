623815e9ccdd1cf0007feaa1a6d71a6170796e4e
Marc Lloyd <marc.lloyd@oneresult.co.uk>
2014-01-27 16:30:37 +0000

    first commit

---
+++ app/index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+//1) Copy package.json and Gruntfile.js to root folder
+//2) Download and unzip latest Wordpress version in to root
+//3) Download html5blank theme and move to themes folder using theme name entered
+//4) Download Advanced Custom Fields and move to plugins folder
+//5) Move wp-config file to root and update with DB details entered
+//6) Move reset stylsheet to theme folder and update theme name (Using a separate reset file to the downloaded one so that we can update the theme name)
+
+
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var DevpressGenerator = module.exports = function DevpressGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(DevpressGenerator, yeoman.generators.Base);
+
+DevpressGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [
+    {
+        type: 'input',
+        name: 'dbName',
+        message: 'Database Name',
+    },
+    {
+        type: 'input',
+        name: 'dbUser',
+        message: 'Database Username',
+        default: 'root'
+    },
+    {
+        type: 'input',
+        name: 'dbPass',
+        message: 'Database Password',
+        default: 'root'
+    },
+      {
+          type: 'input',
+          name: 'themeName',
+          message: 'Enter a name for your theme (The default twentyfourteen theme will be downloaded and renamed)',
+          default: ''
+      }
+  ];
+
+    this.prompt(prompts, function (props) {
+        this.dbName = props.dbName;
+        this.dbUser = props.dbUser;
+        this.dbPass = props.dbPass;
+        this.installTheme = props.installTheme;
+        this.themeName = props.themeName;
+
+        if(!this.themeName){
+            this.themeName = 'twentyfourteen';
+        }
+
+    cb();
+  }.bind(this));
+};
+
+DevpressGenerator.prototype.gruntFiles = function gruntFiles() {
+    this.copy('_package.json', 'package.json');
+    this.copy('_Gruntfile.js', 'Gruntfile.js');
+};
+
+DevpressGenerator.prototype.LatestWordpress = function LatestWordpress() {
+    var cb   = this.async()
+        , self = this
+
+    this.log.writeln('Let\'s download the latest Wordpress Version.')
+    this.tarball('https://github.com/WordPress/WordPress/archive/master.tar.gz', './', cb)
+};
+
+DevpressGenerator.prototype.twentyfourteenTheme = function twentyfourteenTheme() {
+
+    var cb   = this.async()
+        , self = this
+    this.log.writeln('Let\'s download the html5blank Wordpress dev theme.')
+    this.tarball('http://wordpress.org/themes/download/twentyfourteen.1.0.zip', 'wp-content/themes/' + this.themeName, cb)
+
+};
+
+DevpressGenerator.prototype.acfWordpress = function acfWordpress() {
+    var cb   = this.async()
+        , self = this
+
+    this.log.writeln('Now we\'ll download the latest advanced custom fields and add it to the plugins folder.')
+    this.tarball('https://github.com/elliotcondon/acf/archive/master.tar.gz', 'wp-content/plugins/advanced-custom-fields', cb)
+};
+
+DevpressGenerator.prototype.updateWpConfig = function updateWpConfig() {
+    this.copy('wp-config.php.tmpl', 'wp-config.php');
+};
+
+//move css template and update theme name
+DevpressGenerator.prototype.moveCss = function moveCss() {
+    this.copy('_style.css', 'wp-content/themes/' + this.themeName + '/style.css');
+};
+
+//remove default themes
+DevpressGenerator.prototype.removeThemes = function moveCss() {
+    this.copy('_style.css', 'wp-content/themes/' + this.themeName + '/style.css');
+};
---
+++ app/templates/_Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+            main: {
+                files: [
+
+                    // includes files within path
+                    {src: ['./**',
+                        '!wp-content/themes/<%= themeName %>/css/**',
+                        '!wp-content/themes/<%= themeName %>/js/**',
+                        '!./node_modules/**',
+                        '!./Gruntfile.js',
+                        '!./npm-debug.log',
+                        '!./package.json',
+                        '!dist/**'
+                    ], dest: 'dist/'},
+
+
+                ]
+            }
+        },
+        imagemin: {
+            png: {
+                options: {
+                    optimizationLevel: 7
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'wp-content/themes/<%= themeName %>/images/',
+                    src: ['**/*.png'],
+                    dest: 'dist/wp-content/themes/<%= themeName %>/images/',
+                    ext: '.png'
+                }]
+            },
+            jpg: {
+                options: {
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'wp-content/themes/<%= themeName %>/images/',
+                    src: ['**/*.jpg'],
+                    dest: 'dist/wp-content/themes/<%= themeName %>/images/',
+                    ext: '.jpg'
+                }]
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'dist/wp-content/themes/<%= themeName %>/js/scripts.js': ['wp-content/themes/<%= themeName %>/js/**.js']
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'dist/wp-content/themes/<%= themeName %>/style.css': ['wp-content/themes/<%= themeName %>/style.css']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Using the 'grunt development' commando will autoprefix, compile sass, concatenate and activate the watch task
+    grunt.registerTask('default', ['copy', 'uglify', 'imagemin', 'cssmin']);
+
+};
